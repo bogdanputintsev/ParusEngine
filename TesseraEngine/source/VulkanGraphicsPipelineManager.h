e7a2b64a2376c230de6be5fc170e10baccb78868
Bogdan Putintsev <bogdan.putintsev@post.cz>
2025-06-19 11:15:07 +0200

    Fixed function stage in graphics pipeline implemented.

--- TesseraEngine/source/VulkanGraphicsPipelineManager.h
+++ TesseraEngine/source/VulkanGraphicsPipelineManager.h
@@ -2,6 +2,7 @@
 #include <memory>
 #include <vector>
 #include <vulkan/vulkan_core.h>
+#include "VulkanSwapChainManager.h"
 
 namespace tessera::vulkan
 {
@@ -9,11 +10,12 @@ namespace tessera::vulkan
 	class VulkanGraphicsPipelineManager
 	{
 	public:
-		void init(const std::shared_ptr<const VkDevice>& device);
+		void init(const std::shared_ptr<const VkDevice>& device, const SwapChainImageDetails& swapChainImageDetails);
 		void clean(const std::shared_ptr<const VkDevice>& device) const;
 	private:
 		static VkShaderModule createShaderModule(const std::vector<char>& code, const std::shared_ptr<const VkDevice>& device);
 
+		VkPipelineLayout pipelineLayout = VK_NULL_HANDLE;
 		VkShaderModule vertexShaderModule = VK_NULL_HANDLE;
 		VkShaderModule fragmentShaderModule = VK_NULL_HANDLE;
 	};
