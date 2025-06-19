7e9ab3260ef2181d6a9ccc3a0474392c3de7097d
Bogdan Putintsev <bogdan.putintsev@post.cz>
2025-06-19 11:15:28 +0200

    Vulkan refactoring with new Importer classes and Storage in world context.

--- TesseraEngine/source/utils/Utils.h
+++ TesseraEngine/source/utils/Utils.h
@@ -5,28 +5,9 @@
 #include <fstream>
 #include <limits>
 
-namespace tessera
+namespace tessera::utils
 {
-
-	// inline std::string generateHash()
-	// {
-	// 	// Get current time in nanoseconds
-	// 	const auto now = std::chrono::high_resolution_clock::now().time_since_epoch();
-	// 	const auto nanoseconds = std::chrono::duration_cast<std::chrono::nanoseconds>(now).count();
-	//
-	// 	// Generate a random number
-	// 	std::random_device rd;
-	// 	std::mt19937_64 gen(rd());
-	// 	std::uniform_int_distribution<unsigned long long> dis(0, std::numeric_limits<unsigned long long>::max());
-	// 	const auto randomNumber = dis(gen);
-	//
-	// 	// Combine datetime and random number to generate hash
-	// 	std::ostringstream oss;
-	// 	oss << std::hex << std::setfill('0') << std::setw(16) << nanoseconds << std::setw(16) << randomNumber;
-	//
-	// 	return oss.str();
-	// }
-
+	
 	inline std::vector<char> readFile(const std::string& filename)
 	{
 		std::ifstream file(filename, std::ios::ate | std::ios::binary);
@@ -41,6 +22,56 @@ namespace tessera
 		file.close();
 
 		return buffer;
+	}
+
+	namespace string
+	{
+		inline std::string toUpperCase(const std::string& input)
+		{
+			std::string result = input;
+			std::ranges::transform(result.begin(), result.end(), result.begin(),
+				[](const char c)
+				{
+					return static_cast<char>(std::tolower(c));
+				});
+			return result;
+		}
+
+		inline std::string toLowerCase(const std::string& input)
+		{
+			std::string result = input;
+			std::ranges::transform(result.begin(), result.end(), result.begin(),
+			    [](const char c)
+			    {
+				    return static_cast<char>(std::tolower(c));
+			    });
+			return result;
+		}
+		
+		inline bool equalsIgnoreCase(const std::string& oneString, const std::string& anotherString)
+		{
+			return toLowerCase(oneString) == toLowerCase(anotherString);
+		}
+		
+	}
+	
+	inline std::string generateHash()
+	{
+		// Get current time in nanoseconds
+		const auto now = std::chrono::high_resolution_clock::now().time_since_epoch();
+		const auto nanoseconds = std::chrono::duration_cast<std::chrono::nanoseconds>(now).count();
+	
+		// Generate a random number
+		std::random_device rd;
+		std::mt19937_64 gen(rd());
+		std::uniform_int_distribution<unsigned long long> dis(0, std::numeric_limits<unsigned long long>::max());
+		const auto randomNumber = dis(gen);
+	
+		// Combine datetime and random number to generate hash
+		std::ostringstream oss;
+		oss << std::hex << std::setfill('0') << std::setw(16) << nanoseconds << std::setw(16) << randomNumber;
+	
+		return oss.str();
 	}
 	
 }
---
+++ TesseraEngine/source/world/Storage.cpp
@@ -0,0 +1,66 @@
+﻿#include "Storage.h"
+
+namespace tessera
+{
+    void Storage::addNewTexture(const std::string& path, const std::shared_ptr<Texture>& newTexture)
+    {
+        std::lock_guard lock(texturesMutex);
+        textures.insert_or_assign(path, newTexture);        
+    }
+
+    std::shared_ptr<Texture> Storage::getTexture(const std::string& path)
+    {
+        std::lock_guard lock(texturesMutex);
+        return textures[path];
+    }
+
+    bool Storage::containsTexture(const std::string& path) const
+    {
+        std::lock_guard lock(texturesMutex);
+        return textures.contains(path);
+    }
+
+    std::vector<std::shared_ptr<Texture>> Storage::getTextures() const
+    {
+        std::vector<std::shared_ptr<Texture>> allTextures;
+
+        {
+            std::lock_guard lock(texturesMutex);
+            allTextures.reserve(textures.size());
+            for (const auto& [key, texture] : textures)
+            {
+                allTextures.push_back(texture);
+            }
+        }
+        
+        return allTextures;
+    }
+
+    void Storage::addNewMesh(const std::string& path, const std::shared_ptr<Mesh>& newMesh)
+    {
+        std::lock_guard lock(meshesMutex);
+        meshes.insert_or_assign(path, newMesh);   
+    }
+
+    std::shared_ptr<Mesh> Storage::getMeshByPath(const std::string& path)
+    {
+        std::lock_guard lock(meshesMutex);
+        return meshes[path];
+    }
+
+    std::vector<std::shared_ptr<Mesh>> Storage::getMeshes() const
+    {
+        std::vector<std::shared_ptr<Mesh>> allMeshes;
+
+        {
+            std::lock_guard lock(meshesMutex);
+            allMeshes.reserve(meshes.size());
+            for (const auto& [key, mesh] : meshes)
+            {
+                allMeshes.push_back(mesh);
+            }
+        }
+        
+        return allMeshes;
+    }
+}
