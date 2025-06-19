483dede3b22abbb7db4c39e7508d52a1e6a58e46
Bogdan Putintsev <bogdan.putintsev@post.cz>
2025-06-19 11:15:24 +0200

    Console GUI implemented

---
+++ TesseraEngine/source/graphics/gui/ConsoleGui.cpp
@@ -0,0 +1,56 @@
+﻿#include "ConsoleGui.h"
+
+#include "Event.h"
+#include "imgui.h"
+#include "core/Input.h"
+
+namespace tessera::imgui
+{
+    void ConsoleGui::registerEvents()
+    {
+        REGISTER_EVENT(EventType::EVENT_KEY_PRESSED, [&](const KeyButton key)
+        {
+           if (key == KeyButton::KEY_GRAVE)
+           {
+               isVisible = !isVisible;
+           }
+        });
+    }
+
+    void ConsoleGui::draw()
+    {
+        if (isVisible)
+        {
+            ImGui::SetNextWindowPos(ImVec2(0, 0));
+            ImGui::SetNextWindowSize(ImVec2(250, ImGui::GetTextLineHeight() * 19));
+            ImGui::SetNextWindowBgAlpha(0.1f);
+            ImGui::Begin("Console", nullptr, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoTitleBar);
+            {
+                ImGui::SetNextItemWidth(-FLT_MIN); 
+                if (ImGui::InputText("##command", commandLineText.data(), commandLineText.capacity() + 1))
+                {
+                    commandLineText.resize(strlen(commandLineText.data()));
+                }
+                
+                if (ImGui::IsItemDeactivatedAfterEdit())
+                {
+                    onNewCommandSent();
+                }
+                
+                ImGui::InputTextMultiline("##console", consoleHistory.data(), consoleHistory.capacity() + 1,
+                    ImVec2(-FLT_MIN, ImGui::GetTextLineHeight() * 16),
+                    ImGuiInputTextFlags_ReadOnly);
+            }
+            ImGui::End();
+        }
+    }
+
+    void ConsoleGui::onNewCommandSent()
+    {
+        if (!commandLineText.empty())
+        {
+            consoleHistory += "> " + commandLineText + "\n";
+            commandLineText = "";
+        }
+    }
+}
